51adbeeb67dc3b256c0fe58660f9ff7d50049a5a
Ben Covi <ben_covi@intuit.com>
2018-10-24 16:57:20 -0700

    still trying to make page less smart

--- assets/js/headdumb.js
+++ assets/js/headdumb.js
@@ -3,12 +3,12 @@
     var contentElement = $(this)
     setHeaders()
     function setHeaders() {
-      contentElement.children("h1").addClass("header-level-1")
-      contentElement.children("h2").addClass("header-level-2")
-      contentElement.children("h3").addClass("header-level-3")
-      contentElement.children("h4").addClass("header-level-4")
-      contentElement.children("h5").addClass("header-level-5")
-      contentElement.children("h6").addClass("header-level-6")
+      contentElement.find("h1").addClass("header-level-1")
+      contentElement.find("h2").addClass("header-level-2")
+      contentElement.find("h3").addClass("header-level-3")
+      contentElement.find("h4").addClass("header-level-4")
+      contentElement.find("h5").addClass("header-level-5")
+      contentElement.find("h6").addClass("header-level-6")
     }
   }
 })(jQuery)
